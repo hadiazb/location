345929237cf209e949bcdf930fddd7e0ca71e558
hadiazb <hadiazb@unal.edu.co>
2024-01-10 13:07:26 -0500

    Star project

---
+++ src/test/java/com/hadiazb/location/location/LocationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hadiazb.location.location;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
